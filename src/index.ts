0db867b23593b209ead8608e8f2449b2decd2b24
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-05-19 19:13:43 +0200

    Add a setting for the fields visibility, and a locker if it is hidden from page config

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
 import { SecretsManager } from './manager';
 import { ISecretsManager } from './token';
 import { InMemoryConnector } from './connectors';
+import { PageConfig } from '@jupyterlab/coreutils';
 
 /**
  * A basic secret connector extension, that should be disabled to provide a new
@@ -23,18 +26,58 @@ const inMemoryConnector: JupyterFrontEndPlugin<void> = {
 /**
  * The secret manager extension.
  */
-const manager: JupyterFrontEndPlugin<ISecretsManager> = {
+const managerPlugin: JupyterFrontEndPlugin<ISecretsManager> = {
   id: 'jupyter-secrets-manager:manager',
   description: 'A JupyterLab extension to manage secrets.',
   autoStart: true,
   provides: ISecretsManager,
-  activate: (app: JupyterFrontEnd): ISecretsManager => {
-    console.log('JupyterLab extension jupyter-secrets-manager is activated!');
-    return new SecretsManager();
+  optional: [ISettingRegistry],
+  activate: (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry
+  ): ISecretsManager => {
+    // Check if the fields are hidden from page config.
+    let showSecretFieldsConfig = true;
+    if (PageConfig.getOption('secretsManager-showFields') === 'false') {
+      showSecretFieldsConfig = false;
+    }
+
+    const manager = new SecretsManager({
+      showSecretFields: showSecretFieldsConfig
+    });
+
+    settingRegistry
+      .load(managerPlugin.id)
+      .then(settings => {
+        // If the fields are hidden from the manager, remove the setting.
+        if (!showSecretFieldsConfig) {
+          delete settings.schema.properties?.['ShowSecretFields'];
+          return;
+        }
+
+        // Otherwise listen to it to update the field visibility.
+        const updateFieldVisibility = () => {
+          const showSecretField =
+            settings.get('ShowSecretFields').composite ?? true;
+          manager.secretFieldsVisibility = showSecretField as boolean;
+        };
+
+        settings.changed.connect(() => updateFieldVisibility());
+        updateFieldVisibility();
+      })
+      .catch(reason => {
+        console.error(
+          `Failed to load settings for ${managerPlugin.id}`,
+          reason
+        );
+      });
+
+    console.debug('JupyterLab extension jupyter-secrets-manager is activated!');
+    return manager;
   }
 };
 
 export * from './connectors';
 export * from './manager';
 export * from './token';
-export default [inMemoryConnector, manager];
+export default [inMemoryConnector, managerPlugin];
